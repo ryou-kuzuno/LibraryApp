47fc06ce7edbaf7cdf99f0f5b230e10bbdacbfff
ryou_kuzuno <ryou0933@gmail.com>
2018-12-26 19:14:38 +0900

    herokuまえ

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,11 +3,11 @@ class ApplicationRecord < ActiveRecord::Base
   #/app/models/　はdb:migrate不要
 
   # Ruby on Rails 検索機能拡張 (railsチュートリアル)
-  def self.search(search) #ここでのself.はUser.を意味する
+  def self.search(search) #self.はBook.
     if search
       where(['content LIKE ?', "%#{search}%"]) #検索とcontentの部分一致を表示
     else
-      all #全て表示。User.は省略
+      all #全て表示
     end
   end
 end
