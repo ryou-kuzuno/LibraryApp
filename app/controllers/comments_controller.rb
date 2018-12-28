3061b8ce7d66aed90547934af6055f071acf777a
ryou_kuzuno <ryou0933@gmail.com>
2018-12-28 10:32:42 +0900

    pull requestのつもり

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,26 @@ class CommentsController < ApplicationController
   # book.controller.rbにある。
   # newとsaveの自動推測
   def create
-    @new_comment = Comment.create params.require(:comment).permit(:book_id, :comment, :user_id)
+    book_id = params[:book_id].to_i
+    comment = params[:comment]
+    user_id = params[:user_id].to_i
+    comment = Comment.new
+    comment.book_id = book_id
+    comment.comment = comment
+    comment.user_id = user_id
+    if comment.save
+      current_comment = Comment.find_by(comment: comment)
+      success_json = {
+        "comment" => current_comment,
+      }
+      render :status => :ok, :json => success_json
+    else
+      failer_json_object = {'status' => 'failer'}
+      render :status => :internal_server_error, :json => failer_json_object
+    end
+    # @new_comment = Comment.create params.require(:comment).permit(:book_id, :comment, :user_id)
 
-    redirect_to "/show/#{params[:comment][:book_id]}"
+    # redirect_to "/show/#{params[:comment][:book_id]}"
   end
 
   #コメントを削除するアクション
