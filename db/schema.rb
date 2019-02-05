981fbee515a6d2790382cad86af9d845be2b9221
TomoakiTANAKA <tanaka1987@gmail.com>
2019-02-06 00:26:23 +0900

    userプロパティ動くように

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_08_024653) do
+ActiveRecord::Schema.define(version: 2019_02_05_151424) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -69,10 +69,11 @@ ActiveRecord::Schema.define(version: 2018_12_08_024653) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nicename"
+    t.string "password"
     t.string "mail"
-    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
